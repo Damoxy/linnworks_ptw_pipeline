cd659784804463ac9b526cc40785839792c947eb
Damoxy <damoxy12@gmail.com>
2025-11-06 15:21:55 +0000

    revert: undo joins to fix duplicate issues

--- etl/FinalOrderItems.sql
+++ etl/FinalOrderItems.sql
@@ -17,56 +17,55 @@ SELECT DISTINCT
         WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
             THEN OI.SubItemSKU
         ELSE OI.ParentSKU
-    END AS final_sku,
-    
+    END AS Final_sku,
+
     CASE 
-        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' AND OI.SubItemQty <> 0
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
+             AND OI.SubItemQty <> 0
             THEN OI.SubItemQty
         ELSE OI.ParentQty
-    END AS final_quantity,
-    
+    END AS Final_quantity,
+
     CASE 
-        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' AND OI.SubItemSellPrice <> 0
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
+             AND OI.SubItemSellPrice <> 0
             THEN OI.SubItemSellPrice
         ELSE OI.ParentSellPrice
-    END AS final_price,
-    
+    END AS Final_price,
+
     CASE 
-        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' AND OI.SubItemUnitCost <> 0
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
+             AND OI.SubItemUnitCost <> 0
             THEN OI.SubItemUnitCost
         ELSE OI.ParentUnitCost
-    END AS final_cost,
-    
-    (
-        CASE 
-            WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
-                 AND OI.SubItemSellPrice <> 0 AND OI.SubItemQty <> 0
-                THEN OI.SubItemSellPrice * OI.SubItemQty
-            ELSE OI.ParentSellPrice * OI.ParentQty
-        END
-    ) AS TotalFinalPrice,
-    
-    (
-        CASE 
-            WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
-                 AND OI.SubItemUnitCost <> 0 AND OI.SubItemQty <> 0
-                THEN OI.SubItemUnitCost * OI.SubItemQty
-            ELSE OI.ParentUnitCost * OI.ParentQty
-        END
-    ) AS TotalFinalCost,
-    
-    O.dProcessedOn AS final_date,
-    
+    END AS Final_cost,
+
+    CASE 
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
+             AND OI.SubItemSellPrice <> 0 AND OI.SubItemQty <> 0
+            THEN OI.SubItemSellPrice * OI.SubItemQty
+        ELSE OI.ParentSellPrice * OI.ParentQty
+    END AS TotalFinalPrice,
+
+    CASE 
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
+             AND OI.SubItemUnitCost <> 0 AND OI.SubItemQty <> 0
+            THEN OI.SubItemUnitCost * OI.SubItemQty
+        ELSE OI.ParentUnitCost * OI.ParentQty
+    END AS TotalFinalCost,
+
+    O.dProcessedOn AS Final_date,
+
     CASE
         WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
             THEN OI.ParentSKU
         ELSE NULL
-    END AS kitsku,
-    
+    END AS KitSKU,
+
     'linnworks' AS source,
     OI.ParentTitle,
     OI.OrderId,
     OI.LocationId
-FROM [linnworks].[lw].[OrderItem_full] OI
-LEFT JOIN [linnworks].[staging].[processed_orders] O
+FROM [linnworks].[staging].[processed_orders] O
+JOIN [linnworks].[lw].[OrderItem_full] OI
     ON O.pkOrderId = OI.OrderId;
--- etl/OrderSales.sql
+++ etl/OrderSales.sql
@@ -22,13 +22,11 @@ INSERT INTO [linnworks].[lw].[Order_sales] (
 SELECT DISTINCT
     TRY_CAST(O.dReceivedDate AS DATETIME) AS OrderDate,
     TRY_CAST(O.dProcessedOn AS DATETIME) AS DispatchDate,
-
-    CASE
-        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> ''
+    CASE 
+        WHEN OI.SubItemSKU IS NOT NULL AND LTRIM(RTRIM(OI.SubItemSKU)) <> '' 
             THEN OI.SubItemSKU
         ELSE OI.ParentSKU
     END AS itemID,
-   
     OI.ParentUnitCost AS UnitCost,
     OI.ParentUnitCost * OI.ParentQty AS TotalCost,
     OI.ParentSellPrice AS UnitPrice,
@@ -37,18 +35,14 @@ SELECT DISTINCT
     OI.ParentTax AS TotalTax,
     OI.ParentTotalIncTax AS TotalIncTax,
     OI.ParentQty AS Quantity,
-   
     OI.SubItemSKU AS SubItemSKU,
     OI.SubItemUnitCost AS SubItemUnitCost,
     OI.SubItemQty AS SubItemQty,
-   
     CAST(OI.OrderId AS VARCHAR(36)) AS fkOrderId,
     OI.LocationId AS FkLocationId,
     OI.ParentItemSource AS ItemSource,
     CAST(OI.ParentStockItemId AS VARCHAR(36)) AS fkStockItemId,
     'linnworks' AS source
-FROM
-    [linnworks].[lw].[OrderItem_full] OI
-LEFT JOIN
-    [linnworks].[staging].[processed_orders] O
-    ON CAST(O.pkOrderID AS VARCHAR(255)) = CAST(OI.OrderId AS VARCHAR(255));
+FROM [linnworks].[staging].[processed_orders] O
+LEFT JOIN [linnworks].[lw].[OrderItem_full] OI
+    ON O.pkOrderID = CAST(OI.OrderId AS VARCHAR(255));
